468a0e2f00c8221e8d68d64fe44260fce9a7d4d7
Sirisha Sadhu <sirishasadhu@Sirishas-MacBook-Air.local>
2024-09-26 13:36:59 +0530

    changes in code

--- Server/Index.js
+++ Server/Index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import userRoutes from "./src/Routers/patientRoutes.js";
+import userRoutes from "./src/Routers/user.Routes.js";
 
 import express from "express";
 import connectMongodb from "./src/config/connectMongodb.js";
@@ -7,40 +7,24 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./src/middleware/errorHandler.middleware.js";
 
-import doctorRouter from "./src/Repository/doctors/doctor.router.js";
-
-dotenv.config();
-
-const app = express();
-
-
-//====== used middleware for parsing req body and cookies =====//
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
-
 //====== setup cors =======//
 const corsOptions = {
   origin: 'http://localhost:3000', //------- Adjust this to your frontend's origin ---------//
   credentials: true,
 };
 
-app.use(cors(corsOptions))
+dotenv.config();
 
-
-//======= setup routes for doctors =======================//
-app.use("/api/doctor", doctorRouter)
-
-
-
-
-
-//======= handle error using errorHandler middleware =========//
-app.use(errorHandler);
-
+const app = express();
 const port = process.env.PORT || 3000;
 
+//====== used middleware for parsing req body and cookies =====//
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+app.use(cors(corsOptions))
+app.use(errorHandler);
 
 app.use("/user", userRoutes);
 
--- Server/src/Routers/doctorRoutes.js
+++
@@ -1,9 +0,0 @@
-// import express from 'express'
-
-// const router = express.Router();
-
-// router.route('/register').post(doctorRegister);
-// router.route('/login').post(doctorLogin);
-
-
-// export default router;
