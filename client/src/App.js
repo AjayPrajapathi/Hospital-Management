e2f8f69b2a4edd877c569711a0e75b54362cf13c
Harikirannaik <harikiran1252@gmail.com>
2024-09-28 23:37:04 +0530

    credentials validation updated in cign up page. still WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 // import DoctorList from './Components/Doctor/DoctorList';
-import Appointment from './Components/Appointments/Appointments';
-import DoctorsList from './Components/Doctor/DoctorsList';
-import Inventory from './Components/Inventory/Inventory';
-import PatientList from './Components/Patient/PatientList';
+// import Appointment from './Components/Appointments/Appointments';
+// import DoctorsList from './Components/Doctor/DoctorsList';
+// import Inventory from './Components/Inventory/Inventory';
+// import PatientList from './Components/Patient/PatientList';
 import LoginPage from './Components/Homepage/LoginPage';
-
 
 // import LoginPage from './Components/Homepage/LoginPage';
 
 function App() {
   return (
-    <LoginPage />
+    
+    <main className='App'>
+      <LoginPage/>
+    </main>
+    // <LoginPage />
 
     // <DoctorsList/>
     // <Inventory/>
--- client/src/Components/Homepage/LoginPage.js
+++ client/src/Components/Homepage/LoginPage.js
@@ -1,29 +1,173 @@
-import React from 'react'
-import './LoginPage.css'
+import React from "react";
+import "./LoginPage.css";
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+  faIcons
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_ ]{3,23}$/;
+const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%])[A-Za-z\d!@#$%]{8,24}$/;
+
 
 const LoginPage = () => {
+  const userRef = useRef();
+  const emailRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPWD] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
+
+  useEffect(() => {
+    const result = EMAIL_REGEX.test(email);
+    console.log(result);
+    console.log(email);
+    setValidEmail(result);
+  }, [email]);
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(pwd);
+    console.log(result);
+    console.log(pwd);
+    setValidPWD(result);
+    const match = pwd === matchPwd;
+    setValidMatch(match);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd, matchPwd]);
+
+
   return (
     <div className="container">
       <div className="form-container">
+        <p ref={errRef} className={errMsg ? "errMsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
         <h1>Get Started Now</h1>
         <form>
           <div className="input-group">
-            <label htmlFor="name">Name</label>
-            <input id="name" placeholder="Enter your name" type="text" />
+            <label htmlFor="name">
+              Name
+              <span className={validName ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
+              </span>
+              <span className={validName || !user ? "hide" : "invalid"}>
+                <FontAwesomeIcon icon={faTimes} />
+              </span>
+            </label>
+            <input
+              type="text"
+              id="name"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              required
+              aria-invalid={validName ? "false" : "true"}
+              aria-describedby="uidnote"
+              onFocus={() => setUserFocus(true)}
+              onBlur={() => setUserFocus(false)}
+              placeholder="Enter your name"
+            />
+            <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+              <FontAwesomeIcon icon={faInfoCircle} />
+              4 to 24 characters.<br />
+              Must begin with a letter.<br />
+              Letters, numbers, underscores, hyphens allowed.
+            </p>
           </div>
           <div className="input-group">
-            <label htmlFor="email">Email address</label>
-            <input id="email" placeholder="Enter your email" type="email" />
+            <label htmlFor="email">
+              Email address
+              <span className={validEmail ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
+              </span>
+              <span className={validEmail || !email ? "hide" : "invalid"}>
+                <FontAwesomeIcon icon={faTimes} />
+              </span>
+            </label>
+            <input
+              id="email"
+              placeholder="Enter your email"
+              type="email"
+              ref={emailRef}
+              autoComplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              aria-invalid={validEmail ? "false" : "true"}
+              aria-describedby="emailnote"
+              onFocus={() => setEmailFocus(true)}
+              onBlur={() => setEmailFocus(false)}
+            />
+            <p id="emailnote" className={emailFocus && email && !validEmail ? "instructions" : "offscreen"}>
+              <FontAwesomeIcon icon={faInfoCircle} />
+              example@domain.com<br />
+            </p>
           </div>
           <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input id="password" placeholder="Enter your password" type="password" />
+            <label htmlFor="password">
+              Password
+              <span className={validPwd ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
+              </span>
+              <span className={validPwd || !pwd ? "hide" : "invalid"}>
+                <FontAwesomeIcon icon={faTimes} />
+              </span>
+            </label>
+            <input
+              id="password"
+              placeholder="Enter your password"
+              type="password"
+              onChange={(e) => setPwd(e.target.value)}
+              required
+              aria-invalid={validPwd ? "false" : "true"}
+              aria-describedby="pwdnote"
+              onFocus={() => setPwdFocus(true)}
+              onBlur={() => setPwdFocus(false)}
+            />
+            <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+              <FontAwesomeIcon icon={faInfoCircle} />
+              8 to 24 characters.<br />
+              Must include uppercase and lowercase letters, a number and a special characte. <br/>
+            </p>
           </div>
           <div className="checkbox-group">
             <input id="terms" type="checkbox" />
             <label htmlFor="terms">I agree to the terms & policy</label>
           </div>
-          <button className="btn" type="submit">Signup</button>
+          <button className="btn" type="submit">
+            Signup
+          </button>
 
           <div className="divider">
             <span>Or</span>
@@ -49,6 +193,6 @@ const LoginPage = () => {
       <div className="image-container"></div>
     </div>
   );
-}
+};
 
 export default LoginPage;
